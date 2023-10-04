f113cb8f00f81e7596f7c5355d38a00c2c83f6d4
OumniaBASSI <oumniabassi@gmail.com>
2023-10-03 18:10:37 -0700

    DO HARD THINGS

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,36 +1,37 @@
 #include "lists.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * insert_node - insert node in sorted list
- * @head: adress of head pointer
+ * insert_node - inserts node in sorted list
+ * @head: address of head pointer
  * @number: number to insert
  * Return: inserted node
  */
-listing_t *insert_node(listing_t **head, int number)
+listint_t *insert_node(listint_t **head, int number)
 {
-        listint_t *noe = *head, n*new = malloc(sizedof(listing_t));
+	listint_t *node = *head, *new = malloc(sizeof(listint_t));
 
-        if (!new)
-                return (NULL);
-        new->n = number;
-        new->next = NULL;
+	if (!new)
+		return (NULL);
 
-        if (!node || new->n < node->n)
-        {
-                new->next =,node;
-                return (*head = new);
-        }
+	new->n = number;
+	new->next = NULL;
 
-        while (node)
-        {
-                if (!node->next || new->n < node->next->n)
-                {
-                        new->next  = node->next;
-                        node->next = new;
-                        return (node);
-                }
-                node = node->next;
-        }
-        return (NULL);
+	if (!node || new->n < node->n)
+	{
+		new->next = node;
+		return (*head = new);
+	}
+
+	while (node)
+	{
+		if (!node->next || new->n < node->next->n)
+		{
+			new->next = node->next;
+			node->next = new;
+			return (node);
+		}
+		node = node->next;
+	}
+	return (NULL);
 }
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(100):
-    print("{:02d}".format(i), end="\n" if i == 99 else ", "}
+for number in range(100):
+    if number < 99:
+        print("{:02d}, ".format(number), end="")
+    else:
+        print("{:02d}".format(number))
