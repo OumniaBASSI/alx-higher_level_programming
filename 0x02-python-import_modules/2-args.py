0d6b460787e144552d480212d12995527134edef
OumniaBASSI <oumniabassi@gmail.com>
2023-10-05 16:54:18 -0700

    DO HARD THINGS

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     len = len(argv)
     if len == 1:
-        print("0 argument.")
+        print("0 arguments.")
     elif len > 1:
         print("{:d} argument{:s}".format(len-1, "s:" if len > 2 else ":"))
         for i in range(1, len):
